00353f0f53852452fcce0f6b6233351e1f0412a9
PrabhitDutta <duttaprabhjit6@gmail.com>
2018-03-22 00:26:46 +0530

    fld_cmp/scan 1.0

--- file_folder_cmp.py
+++ file_folder_cmp.py
@@ -12,11 +12,18 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.argument('choice', type=click.INT)
 def cli(path1, path2, choice, verbose):
 
-    '''This Command helps Scan 2 folder tos search for similar/different files or folders in Them.
-        The syntax of this command is fld_cmp [PATH1] [PATH2] 1/2/3 where PATH1 represents the address
-        of one of the folder you want to scan and PATH2 is the address of the folder you want to
-        compare it to and finally you have to type 1 if you want to scan for similar files/folder,
-        2 for different files/folders or 3 to sync the two folders.'''
+    '''This command helps scan 2 folder to search for similar/different files or folders in them and you also have
+            the option to sync those directories.
+
+
+            PATH1 :- Address of one of the folder you want to scan.
+
+
+            PATH2 :- Address of the folder you want to
+            compare it to.
+
+
+            Choice :- Type 1 if you want to scan for similar files/folder, 2 for different files/folders or 3 to sync the two folders.'''
 
     count = 0
     temp = 0

8efb20d919eaa0b606d78eb4f2ccef2c296a60ad
PrabhitDutta <duttaprabhjit6@gmail.com>
2018-03-21 23:27:36 +0530

    fld_cmp 1.0

--- file_folder_cmp.py
+++ file_folder_cmp.py
@@ -1,26 +1,30 @@
 import os
 import click
+from dirsync import sync
+from progressbar import *
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 @click.command()
+@click.option('--verbose', '-v', is_flag=True)
 @click.argument('path1', type=click.Path(exists=True))
 @click.argument('path2', type=click.Path(exists=True))
 @click.argument('choice', type=click.INT)
-def cli(path1, path2, choice, s):
+def cli(path1, path2, choice, verbose):
 
-    '''This Command helps Scan 2 folder to search for similar/different files or folders in Them.
-        The syntax of this command is fld_cmp [PATH1] [PATH2] 1/2 where PATH1 represents the address
+    '''This Command helps Scan 2 folder tos search for similar/different files or folders in Them.
+        The syntax of this command is fld_cmp [PATH1] [PATH2] 1/2/3 where PATH1 represents the address
         of one of the folder you want to scan and PATH2 is the address of the folder you want to
-        compare it to and finally you have to type 1 if you want to scan for similar files/folder
-        or 2 for different files/folders.'''
+        compare it to and finally you have to type 1 if you want to scan for similar files/folder,
+        2 for different files/folders or 3 to sync the two folders.'''
 
     count = 0
     temp = 0
 
     file_list1 = os.listdir(path1)
     file_list2 = os.listdir(path2)
-    result_list = []
+    sync_list1 = []
+    sync_list2 = []
 
     print("\n")
 
@@ -34,7 +38,7 @@ def cli(path1, path2, choice, s):
                     count += 1
         print("\n{} Similar files/folder Found".format(count))
 
-    else:
+    elif choice == 2:
         for i in file_list1:
             for j in file_list2:
                 if i == j:
@@ -47,3 +51,21 @@ def cli(path1, path2, choice, s):
             print(i)
             temp += 1
         print("\n{} Different files/folders Found".format(temp))
+
+    else:
+        for i in file_list1:
+            for j in file_list2:
+                if i == j:
+                    count += 1
+                    file_list2.pop(file_list2.index(j))
+            if count == 0:
+                sync_list1.append(i)
+                temp += 1
+        for i in file_list2:
+            sync_list2.append(i)
+            temp += 1
+
+        for i in file_list1:
+            os.system(f'copy "{path1}\\{i}" "{path2}"')
+        for i in file_list2:
+            os.system(f'copy "{path2}\\{i}" "{path1}"')
